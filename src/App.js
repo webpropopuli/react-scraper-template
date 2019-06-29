c15f7811f5a4fc69b482daa66010a510fdcbd2c3
David Marlowe <webpropopuli@gmail.com>
2019-06-28 22:02:04 -0400

    First pass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import cheerio from "cheerio";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState({ names: [] });
+
+  const URL = "https://pokedex.org/";
+  // use the request-promise library to fetch the HTML from pokemon.org
+  useEffect(() => {
+    (async () => {
+      //# FETCH DATA HERE
+      const result = await axios(URL);
+
+      let names = [];
+
+      //# EXTRACT THE RELEVANT DATA FROM HTML
+      const html = result.data;
+      let $ = cheerio.load(html); //# <- this is the raw html from the fetch
+      try {
+        //# LOOP THROUGH ALL FOUND
+        $("#monsters-list li span").each(function(i, elem) {
+          names.push({ id: i, name: $(this).text() });
+        });
+      } catch (err) {
+        console.log("crawl failed", err);
+      } finally {
+        console.log(`Found ${names.length} pokemons`);
+        //#UPDATE STATE
+        setData({ names: [...names] });
+      }
+    })();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ol>
+        {data.names.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ol>
     </div>
   );
 }
